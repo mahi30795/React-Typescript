21ad11ba06e929ef2329f5bbb8712781d0de2f9b
MaHi <mahesh30795@gmail.com>
2021-01-11 20:35:38 +0530

    more changes

--- src/components/NavBar/NavIcons/NavIcons.style.tsx
+++ src/components/NavBar/NavIcons/NavIcons.style.tsx
@@ -3,14 +3,10 @@ import {makeStyles} from '@material-ui/core/styles';
 const useStyles = makeStyles({
     iconNav:{
         padding: '16px',
-        '& ul':{
-            '& li':{
-                float: 'left',
-                listStyleType:'none'
-            }
-        }
     },
     icon:{
+        float: 'left',
+        listStyleType:'none',
         margin:'0 10px',
         '& img':{
             width: '35px',
